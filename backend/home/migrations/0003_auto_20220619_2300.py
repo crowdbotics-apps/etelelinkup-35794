d45f39dd126a0af84ca292c5b752c73a121dfdac
Crowdbotics <team@crowdbotics.com>
2022-06-19 23:02:18 +0000

    Update models

--- backend/home/migrations/0003_auto_20220619_2300.py
+++ backend/home/migrations/0003_auto_20220619_2300.py
@@ -6,19 +6,27 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('home', '0002_body'),
+        ("home", "0002_body"),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='Entry',
+            name="Entry",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('body', models.CharField(max_length=256)),
-                ('title', models.CharField(max_length=256)),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("body", models.CharField(max_length=256)),
+                ("title", models.CharField(max_length=256)),
             ],
         ),
         migrations.DeleteModel(
-            name='Body',
+            name="Body",
         ),
     ]
